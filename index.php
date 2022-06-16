e6ec1f6085ed289ecb6d08f7bb29420f1b9f19fe
Weastacia <jose.naacho09@gmail.com>
2022-06-16 16:25:51 -0400

    borrado de boton 'Enviar'

--- index.php
+++ index.php
@@ -749,7 +749,6 @@ include_once "conexion.php";
       </form>
     </div>
     </div>
-    <button class="btn btn-primary">Enviar</button>
   </form>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   <script src="./js/scripts.js"></script>
